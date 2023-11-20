fcfa993dde1a93bc842a7edae06edef12999a5d6
OscarSilvaOfficial <oscarkaka222@gmail.com>
2023-11-20 14:18:34 -0300

    fix: command

--- debian/8-install-rust.sh
+++ debian/8-install-rust.sh
@@ -1,2 +1,2 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-echo \\n\# Rust Configs \\nsource "$HOME/.cargo/env" >> ~/.zshrc
+echo \\n\# Rust Configs \\n'source "$HOME/.cargo/env"' >> ~/.zshrc
