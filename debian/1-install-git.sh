7a8f9deef7057d96a818726133e72eba5219a978
Oscar da Silva <oscarkaka222@gmail.com>
2023-11-26 23:52:53 -0300

    Update 1-install-git.sh

--- debian/1-install-git.sh
+++ debian/1-install-git.sh
@@ -1,1 +1,6 @@
 sudo apt install git -y
+
+git config --global user.name "OscarSilvaOfficial"
+git config --global user.email "oscarkaka222@gmail.com"
+
+git config --global credential.helper "cache --timeout=604800"
